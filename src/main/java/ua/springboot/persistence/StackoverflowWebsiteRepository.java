cda499a6b5b66987b16932bbcbb653b80e06154e
Vladyslav Glukhov <vladyslavglukhov@gmail.com>
2017-06-17 23:07:53 +0300

    add method findAllByWebsite

--- src/main/java/ua/springboot/persistence/StackoverflowWebsiteRepository.java
+++ src/main/java/ua/springboot/persistence/StackoverflowWebsiteRepository.java
@@ -2,18 +2,17 @@ package ua.springboot.persistence;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import ua.springboot.model.StackoverflowWebsite;
 
 import java.util.List;
 
 @Repository
-public class StackoverflowWebsiteRepository {
-    @Autowired
-    private MongoTemplate mongoTemplate;
+public interface StackoverflowWebsiteRepository extends MongoRepository<StackoverflowWebsite, String> {
+    List<StackoverflowWebsite> findAllByWebsite(String website);
 
-
-    public List<StackoverflowWebsite> findAll() {
-        return mongoTemplate.findAll(StackoverflowWebsite.class);
-    }
+//    public List<StackoverflowWebsite> findAll() {
+//        return mongoTemplate.findAll(StackoverflowWebsite.class);
+//    }
 }
--- src/main/java/ua/springboot/service/StackoverflowService.java
+++ src/main/java/ua/springboot/service/StackoverflowService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import ua.springboot.model.StackoverflowWebsite;
 import ua.springboot.persistence.StackoverflowWebsiteRepository;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +18,11 @@ public class StackoverflowService {
     static {
         items.add(new StackoverflowWebsite("stackoverflow", "http://stackoverflow.com", "http://www.iconsdb.com/icons/download/orange/stackoverflow-6-64.ico", "Stack Overflow", "for programmers"));
         items.add(new StackoverflowWebsite("superuser", "http://superuser.com", "http://www.iconsdb.com/icons/download/color/CFFF30/linkedin-3-64.ico", "Super User", "for power user"));
+    }
+
+    @PostConstruct
+    public void init() {
+        repository.save(items);
     }
 
     public List<StackoverflowWebsite> findAll() {
