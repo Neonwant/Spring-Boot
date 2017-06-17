3e74c2e9edaa23a2edb422c5417fe62510909635
Vladyslav Glukhov <vladyslavglukhov@gmail.com>
2017-06-17 17:44:02 +0300

    add handler

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,12 +1,9 @@
 var app = angular.module("springDemo", []);
 
-app.controller("AppCtrl", function ($scope) {
-    $scope.websites = [{
-        iconImageUrl: '',
-        id: 'stackoverflow',
-        website: 'stackoverflow.com',
-        title: 'StackOverflow webiste',
-        description: 'StackOverflow description'
-    }];
+app.controller("AppCtrl", function ($scope, $http) {
+    $scope.websites = [];
 
+    $http.get('http://localhost:8099/api/stackoverflow').success(function (data) {
+        $scope.websites = data;
+    });
 });
