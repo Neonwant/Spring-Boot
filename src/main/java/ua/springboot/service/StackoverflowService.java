f9412e2f3b240bbe10c11e9ddc7aa4e34ac59b0a
Vladyslav Glukhov <vladyslavglukhov@gmail.com>
2017-06-17 17:23:35 +0300

    imitation of method findAll()

--- src/main/java/ua/springboot/service/StackoverflowService.java
+++ src/main/java/ua/springboot/service/StackoverflowService.java
@@ -1,8 +1,20 @@
 package ua.springboot.service;
 
 import org.springframework.stereotype.Service;
+import ua.springboot.model.StackoverflowWebsite;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class StackoverflowService {
+    private static List<StackoverflowWebsite> items = new ArrayList<>();
+    static {
+        items.add(new StackoverflowWebsite("stackoverflow", "http://stackoverflow.com", "http://www.iconsdb.com/icons/download/orange/stackoverflow-6-64.ico", "Stack Overflow", "for programmers"));
+        items.add(new StackoverflowWebsite("superuser", "http://superuser.com", "http://www.iconsdb.com/icons/download/color/CFFF30/linkedin-3-64.ico", "Super User", "for power user"));
+    }
 
+    public List<StackoverflowWebsite> findAll() {
+        return items;
+    }
 }
--- src/main/java/ua/springboot/web/StackOverflowController.java
+++ src/main/java/ua/springboot/web/StackOverflowController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.springboot.model.StackoverflowWebsite;
+import ua.springboot.service.StackoverflowService;
 
 import java.util.List;
 
