dd13de9b1d10834c587567d0384d3def232c3fa9
Vladyslav Glukhov <vladyslavglukhov@gmail.com>
2017-06-17 18:02:23 +0300

    fix bug in the handler

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -3,7 +3,13 @@ var app = angular.module("springDemo", []);
 app.controller("AppCtrl", function ($scope, $http) {
     $scope.websites = [];
 
-    $http.get('http://localhost:8099/api/stackoverflow').success(function (data) {
-        $scope.websites = data;
+    $http({
+        method: 'GET',
+        url: 'http://localhost:8099/api/stackoverflow'
+    }).then(function (success) {
+        $scope.websites = success.data;
+    }, function (error) {
+        console.log(error, 'can`t get data');
     });
+
 });
